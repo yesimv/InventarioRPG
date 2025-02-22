78c4988b6b121cdf5b0aa503d37559d610cf2ddd
DreamRemnant <jfmg.64@gmail.com>
2025-02-22 13:44:09 -0700

    Se reparó registro de elementos. Tipo de dato y rareza solo tomaban el primer valor. No guardaba descripcion ni imagen. Funciona correctamente.

--- sistema/procesar_agregar.php
+++ sistema/procesar_agregar.php
@@ -6,15 +6,20 @@ ini_set('display_errors', 1);
 $mensaje = '';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    
     $nombre_producto = $_POST['nombre_producto'];
     $stock = $_POST['stock'];
     $precio_venta = $_POST['precio_venta'];
+    $tipo_equipo = $_POST['tipo'];
+    $descripcion_equipo = $_POST['descripcion'];
+    $rareza_equipo = $_POST['rareza'];
+    $imagen_equipo = $_POST['image'];
     var_dump($_POST);
 
     // Preparar y ejecutar la consulta
-    $sql = "INSERT INTO inventario (nombre_producto, stock, precio_venta) VALUES (?, ?, ?)";
+    $sql = "INSERT INTO inventario (nombre_producto, stock, precio_venta, tipo, descripcion, rareza, image) VALUES (?, ?, ?, ?, ?, ?, ?)";
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("sid", $nombre_producto, $stock, $precio_venta);
+    $stmt->bind_param("sidssss", $nombre_producto, $stock, $precio_venta, $tipo_equipo, $descripcion_equipo, $rareza_equipo, $imagen_equipo);
 
     if ($stmt->execute()) {
         $mensaje = "Producto agregado exitosamente.";
